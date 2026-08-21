be3bc92f5a9412a2f4f3252f1a6b164e51e13d6e
Anna Pitzer <mariaannap2006@gmail.com>
2026-08-21 10:26:06 -0300

    Inicialização do frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import  Header  from "./components/Header";
+import  Navbar  from "./components/Navbar";
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
