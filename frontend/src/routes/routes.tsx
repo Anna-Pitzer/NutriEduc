e798f782c71a940b26755415c395b9abde51ba49
Kayke Silva <kaykestudy307@gmail.com>
2026-08-24 16:07:45 -0300

    Pagina login refeita

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -2,14 +2,19 @@ import { Routes, Route } from "react-router-dom"
 import { LoginPage } from "../pages/login"
 import { HomePage } from "../pages/home"
 import { ProtectedRoutes } from "./protectedRoutes"
+import { PaletaDeCor } from "../pages/colorsTESTE"
 
 export function Rotas() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/colors" element={<PaletaDeCor/>}/>
+      <Route path="/" element={<HomePage />} />
 
+      
       <Route element={<ProtectedRoutes />}>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        
       </Route>
     </Routes>
   )
